58c765aca69f90b3f0a5fa4ecce086ceabd6e295
Mikolaj Piechocki <mikolaj.piechocki@gmail.com>
2020-01-12 15:04:51 +0100

    uzupełnienie

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,17 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] books = new String[7];
+        books [0] = "Niebezpieczne związki Bronisława Komorowskiego";
+        books [1] = "Niebezpieczne związki Donalda Tuska";
+        books [2] = "Niebezpieczne związki Andrzeja Leppera";
+        books [3] = "Komisarz";
+        books [4] = "Powrót do Jedwabnego";
+        books [5] = "Niebezpieczne związki Generała Petelickiego";
+        books [6] = "Rozprawa";
+
+        String book = books [2];
+        System.out.println(book);
+    }
+
+}
+
