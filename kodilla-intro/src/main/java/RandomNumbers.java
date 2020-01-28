5ac3b8df1beff9241b64428fbe6b12f53ce72391
Mikolaj Piechocki <mikolaj.piechocki@gmail.com>
2020-01-28 23:25:30 +0100

    Uzupełnienie próba

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,21 @@
+/*import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        getCountOfRabdomNumber(500);
+    }
+        public static int getCountOfRandomNumber (int max){
+            Random random = new Random();
+            int result = 0;
+            int sum = 0;
+            while (sum < max) {
+                int temp = random.nextInt(10);
+                sum = sum + temp;
+                result++;
+            }
+            System.out.println(result);
+            return;
+        }
+    }
+}*/
+
